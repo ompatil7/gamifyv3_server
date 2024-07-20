a152fd98e4bc0625d43093200e38b62df6a36d5a
ompatil7 <ompatil4749@gmail.com>
2024-07-20 11:22:03 +0530

    initial commit

---
+++ middlewares/protectRoute.js
@@ -0,0 +1,24 @@
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+//next for running the next function after successsfull completions
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password"); //same in payload, - cause do not wan to return password
+
+    req.user = user;
+    //inside req obj we are adding user field which we just got from the database
+
+    //calling next function
+    next();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in protectRoute ", error.message);
+  }
+};
+
+export default protectRoute;
